e414e13e3ad3dfeef2cab6dfc647afa6de4a34c9
TWcamel <j0930495159@gmail.com>
2022-04-21 00:18:09 +0800

    change way to read image files

--- js/buttonIn.js
+++ js/buttonIn.js
@@ -3,18 +3,18 @@ const raindropButton = document.querySelectorAll('.raindrop__button');
 const container = document.querySelector('.container');
 
 const cityImage = {
-    '臺北市': '../images/taipei.jpg',
-    '高雄市': '../images/kaohsiung.jpg',
-    '新北市': '../images/NewTaipeiCity.jpg',
-    '臺中市': '../images/taichung.jpg',
-    '臺南市': '../images/tainan.jpg',
-    '桃園市': '../images/taoyuan.jpg',
+    '臺北市': './images/taipei.jpg',
+    '高雄市': './images/kaohsiung.jpg',
+    '新北市': './images/NewTaipeiCity.jpg',
+    '臺中市': './images/taichung.jpg',
+    '臺南市': './images/tainan.jpg',
+    '桃園市': './images/taoyuan.jpg',
 }
 
 // 預先載入臺北市的背景圖片
 const image = document.createElement('img')
 image.className = "background__image";
-image.src = '../images/taipei.jpg'
+image.src = './images/taipei.jpg'
 container.appendChild(image);
 
 let city
